75b1db04f4f450df458c1b913b10fabb2f9f8d48
Vlad <vlad_sklyarov@mail.ru>
2021-07-06 17:16:24 +0200

    СделалБазовыйЭкранПользовательскогоПрофиля

--- Aviato/Aviato/Views/UserProfileViewController.swift
+++ Aviato/Aviato/Views/UserProfileViewController.swift
@@ -29,12 +29,17 @@ class UserProfileViewController: UIViewController {
         navigationController?.setNavigationBarHidden(true, animated: false)
         setupUsernameLabel()
         setupLogoutButton()
+        setupData()
     }
     
     func setupUsernameLabel() {
         self.view.addSubview(usernameLabel)
         usernameLabel.text = "123456"
         usernameLabel.backgroundColor = .white
+        usernameLabel.layer.cornerRadius = 25
+        usernameLabel.clipsToBounds = true
+        usernameLabel.textAlignment = .center
+        usernameLabel.numberOfLines = 0
         usernameLabel.snp.makeConstraints { (make) in
             make.top.equalToSuperview().offset(100)
             make.leading.equalToSuperview().offset(16)
@@ -48,14 +53,21 @@ class UserProfileViewController: UIViewController {
         logoutButton.setTitle("Выйти", for: .normal)
         logoutButton.backgroundColor = .clear
         logoutButton.setTitleColor(.red, for: .normal)
+        logoutButton.addTarget(self, action: #selector(logout), for: .touchUpInside)
         logoutButton.snp.makeConstraints { (make) in
             make.trailing.equalToSuperview().offset(-16)
             make.top.equalToSuperview().offset(50)
             make.width.equalTo(75)
             make.height.equalTo(50)
         }
-        
     }
     
-
+    @objc func logout() {
+        presenter.logout()
+    }
+    
+    func setupData() {
+        let userInfo: UserViewModel = presenter.getUser() ?? UserViewModel(userID: UUID(), username: "111", password: "111")
+        usernameLabel.text = "Имя пользователя: " + userInfo.username
+    }
 }
